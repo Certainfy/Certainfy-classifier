8a10d367584c9447b3d064ab72b7bf1df61a69b9
Edwin Anajemba <anajembaedwin@gmail.com>
2023-07-10 00:59:06 +0100

    Dev (#12)
    * added server folder
    
    * moved files outward
    
    * setup client structure
    
    * server requirements added
    
    * resolved LF issue
    
    * built chatpage (#9)
    
    * Edwin branch (#11)
    
    * built chatpage
    
    * made chatpage responsive

--- client/pages/ChatPage.jsx
+++ client/pages/ChatPage.jsx
@@ -14,28 +14,31 @@ const ChatPage = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <div className="border border-gray-300 rounded-lg p-4 mb-4">
-        {messages.map((message, index) => (
-          <p key={index} className="mb-2">
-            {message}
-          </p>
-        ))}
-      </div>
-      <div className="flex">
-        <input
-          type="text"
-          className="border border-gray-300 rounded-l-lg p-2 flex-grow"
-          placeholder="Type your message"
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-        />
-        <button
-          className="bg-blue-500 hover:bg-blue-600 text-white rounded-r-lg px-4 py-2"
-          onClick={handleSendMessage}
-        >
-          Send
-        </button>
+    <div className="min-h-screen bg-zinc-600 flex flex-col justify-center items-center px-4 sm:px-8">
+      <h1 className="text-3xl text-center mb-8 text-brown">Welcome to Certainfy-classifier</h1>
+      <div className="max-w-lg w-full bg-white rounded-lg shadow-lg p-4 sm:p-8">
+        <div className="mb-8">
+          {messages.map((message, index) => (
+            <p key={index} className="mb-2 text-gray-800">
+              {message}
+            </p>
+          ))}
+        </div>
+        <div className="flex">
+          <input
+            type="text"
+            className="flex-grow border border-gray-300 rounded-l-lg p-2 sm:px-4 sm:py-2 text-black"
+            placeholder="Type your message"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
+          <button
+            className="bg-zinc-600 hover:bg-zink-dark text-white rounded-r-lg px-4 py-2"
+            onClick={handleSendMessage}
+          >
+            Send
+          </button>
+        </div>
       </div>
     </div>
   );
--- client/pages/index.js
+++ client/pages/index.js
@@ -3,10 +3,10 @@ import ChatPage from './ChatPage';
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome to Certainfy-classifier</h1>
       <ChatPage />
     </div>
   );
 };
 
 export default HomePage;
+
